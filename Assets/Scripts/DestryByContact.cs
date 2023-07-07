6ff02559b0f60409c1802ca4f45beb0c190b9bf8
Nathan Foote <39596241+Nanoparty@users.noreply.github.com>
2023-07-06 22:44:21 -0500

    Initial Upload

---
+++ Assets/Scripts/DestryByContact.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestryByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    public int scoreValue;
+    private GameCtlr gameCtlr;
+
+	// Use this for initialization
+	void Start () {
+        GameObject gameCtlrObject = GameObject.FindWithTag("GameController");
+        if(gameCtlrObject != null)
+        {
+            gameCtlr = gameCtlrObject.GetComponent<GameCtlr>();
+        }
+        else
+        {
+            Debug.Log("GameCtrl Null");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary" || other.tag == "Enemy")
+            return;
+        if(explosion != null)
+            Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameCtlr.GameOver();
+        }
+
+        gameCtlr.addScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
